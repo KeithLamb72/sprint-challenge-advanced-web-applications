8c5f8bfe34a791053244a3fe5e077e61cb9736b8
KeithLamb <keithianlamb@gmail.com>
2024-06-06 13:51:08 -0500

    finish spinner test.js rendering tests for on/off props

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,18 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render } from '@testing-library/react';
+import Spinner from './Spinner';
+
+test('spinner does not render when off', () => {
+  const { container } = render(<Spinner on={false} />);
+  expect(container.querySelector('#spinner')).toBeNull();
+});
+
+test('spinner renders when on', () => {
+  const { container } = render(<Spinner on={true} />);
+  expect(container.querySelector('#spinner')).not.toBeNull();
+  expect(container.querySelector('#spinner').textContent).toContain('Please wait...');
+});
+
+
